72049e9bf2c611f98f0d9431f62be308ea49dff6
Erin Francia <erinfranmc@gmail.com>
2019-02-27 16:04:30 +0000

    hamburger menu works correctly on mobile, colour scheme changes

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-console */
+/* eslint-env browser */
+
 const nav = document.getElementById('navbar-menu');
+const navOverlay = document.getElementById('navbar-overlay');
 const navToggle = document.getElementById('navbar-hamburger');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', () => {
+  console.log('clicked');
   nav.classList.toggle('active');
+  navOverlay.classList.toggle('active');
 });
